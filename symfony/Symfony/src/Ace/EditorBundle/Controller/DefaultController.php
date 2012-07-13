dabafa79b4df557ce105855e8b4c9dc5e69c899b
Vasileios Georgitzikis <billgeo13@gmail.com>
2012-07-14 00:24:28 +0300

    fixed the compiler to use POST requests instead of GET

--- symfony/Symfony/src/Ace/EditorBundle/Controller/DefaultController.php
+++ symfony/Symfony/src/Ace/EditorBundle/Controller/DefaultController.php
@@ -111,7 +111,7 @@ class DefaultController extends Controller
 
 				$data = "ERROR";
 
-				$data = $this->get_data("http://compiler.codebender.cc?data=".urlencode($value));
+				$data = $this->get_data("http://compiler.codebender.cc", 'data', urlencode($value));
 
 				$json_data = json_decode($data, true);
 				if($json_data['success'])
@@ -424,16 +424,20 @@ class DefaultController extends Controller
 		return $url;
 	}
 
-	private function get_data($url)
+	private function get_data($url, $var, $value)
 	{
-	  $ch = curl_init();
-	  $timeout = 10;
-	  curl_setopt($ch,CURLOPT_URL,$url);
-	  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-	  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-	  $data = curl_exec($ch);
-	  curl_close($ch);
-	  return $data;
+		$ch = curl_init();
+		$timeout = 10;
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+
+		curl_setopt($ch,CURLOPT_POST,1);
+		curl_setopt($ch,CURLOPT_POSTFIELDS,$var.'='.$value);
+
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
 	}
 
 	public function userAction($user)
