62c111fd15713333c4f27ea18da82e6238cc8e49
Vasileios Georgitzikis <billgeo13@gmail.com>
2012-08-30 00:29:20 +0300

    improved the cache cleaning script

--- symfony/Symfony/web/clean_cache.php
+++ symfony/Symfony/web/clean_cache.php
@@ -5,22 +5,25 @@ ini_set('display_errors', '1');
 
 //erases a directory recursively
 function rrmdir($dir) {
-if (is_dir($dir)) {
-$objects = scandir($dir);
-foreach ($objects as $object) {
-if ($object != "." && $object != "..") {
-if (filetype($dir . "/" . $object) == "dir")
-{
-	echo("Deleted: $dir/$object<br />\n");
-	rrmdir($dir . "/" . $object);
-}
-else
-unlink($dir . "/" . $object);
-}
-}
-reset($objects);
-// rmdir($dir);
-}
+	if (is_dir($dir))
+	{
+		$objects = scandir($dir);
+		foreach ($objects as $object) {
+		if ($object != "." && $object != "..")
+		{
+			echo("Deleting: $dir$object<br />\n");
+			if (filetype($dir."/".$object) == "dir")
+			{
+				rrmdir($dir."/".$object);
+				rmdir($dir."/".$object);
+			}
+			else
+				unlink($dir."/".$object);
+			}
+		}
+		reset($objects);
+		// rmdir($dir);
+	}
 }
 //erases the cache
 rrmdir('../app/cache/');
