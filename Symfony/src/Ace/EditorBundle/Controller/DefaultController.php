c6c8695554b731346df458ac648e6e1e2f771578
Vasileios Georgitzikis <georgitzik@ceid.upatras.gr>
2012-09-23 21:00:29 +0300

    moved the get_gravatar function to the Utilities

--- Symfony/src/Ace/EditorBundle/Controller/DefaultController.php
+++ Symfony/src/Ace/EditorBundle/Controller/DefaultController.php
@@ -318,34 +318,10 @@ class DefaultController extends Controller
 		{
 			throw $this->createNotFoundException('No user found with id '.$name);
 		}
-		$image = $this->get_gravatar($user->getEmail());
+		$utilities = new DefaultHandler();			
+		$image = $utilities->get_gravatar($user->getEmail());
 
 		return $this->render('AceEditorBundle:Default:image.html.twig', array('user' => $user->getUsername(),'image' => $image));
-	}
-
-	/**
-		* Get either a Gravatar URL or complete image tag for a specified email address.
-		*
-		* @param string $email The email address
-		* @param string $s Size in pixels, defaults to 80px [ 1 - 512 ]
-		* @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
-		* @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
-		* @param boole $img True to return a complete IMG tag False for just the URL
-		* @param array $atts Optional, additional key/value attributes to include in the IMG tag
-		* @return String containing either just a URL or a complete image tag
-		* @source http://gravatar.com/site/implement/images/php/
-	*/
-	private function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() )
-	{
-		$url = 'http://www.gravatar.com/avatar/';
-		$url .= md5( strtolower( trim( $email ) ) );
-		$url .= "?s=$s&d=$d&r=$r";
-		if ( $img ) {
-			$url = '<img src="' . $url . '"';
-			foreach ( $atts as $key => $val ) $url .= ' ' . $key . '="' . $val . '"';
-			$url .= ' />';
-		}
-		return $url;
 	}
 
 	public function userAction($user)
@@ -364,7 +340,8 @@ class DefaultController extends Controller
 		} else {
 			$lastTweet=0;
 		}
-		$image = $this->get_gravatar($user->getEmail(),120);
+		$utilities = new DefaultHandler();			
+		$image = $utilities->get_gravatar($user->getEmail(),120);
 		return $this->render('AceEditorBundle:Default:user.html.twig', array( 'user' => $user, 'files' => $files, 'lastTweet'=>$lastTweet, 'image'=>$image ));
 	}
 	public function projectAction($username, $project_name)
