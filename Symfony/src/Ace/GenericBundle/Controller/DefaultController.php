a52414e063a2f69e1ede906969ff332b87bbeafc
Vasileios Georgitzikis <tzikis@gmail.com>
2012-10-04 03:01:58 +0300

    updated library view to work with the new library listing and description fetching APIs

--- Symfony/src/Ace/GenericBundle/Controller/DefaultController.php
+++ Symfony/src/Ace/GenericBundle/Controller/DefaultController.php
@@ -10,13 +10,16 @@ class DefaultController extends Controller
 	public function librariesAction()
 	{
 		$utilities = $this->get('utilities');
-		$libraries = json_decode($utilities->get_data($this->container->getParameter('library'), 'data', "external"), true);
+		$libraries = json_decode($utilities->get_data($this->container->getParameter('library'), 'data', "list-external"), true);
 		$libraries = $libraries["list"];
 		
-		foreach($libraries as &$libinfo)
+		foreach($libraries as $key=>$library)
 		{
-			if(isset($libinfo["description"]))
-				$libinfo["description"] = $utilities->get($libinfo["description"]["url"]);
+			$libraries[$key] = array("name" => $library);
+			$libinfo = json_decode($utilities->get_data($this->container->getParameter('library'), 'data', "fetch-description-external&name=".$library), true);
+			$libraries[$key]["description"] =  $libinfo["description"];
+			// if(isset($libinfo["description"]))
+			// 	$libinfo["description"] = $utilities->get($libinfo["description"]["url"]);
 		}
 		
 		
