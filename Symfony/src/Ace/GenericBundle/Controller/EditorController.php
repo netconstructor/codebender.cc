683ebeecc6446e817b0a1faa8b663b65660ac710
Vasileios Georgitzikis <tzikis@gmail.com>
2012-10-19 01:18:04 +0300

    added automatic board fetching and dropdown menu generation\!

--- Symfony/src/Ace/GenericBundle/Controller/EditorController.php
+++ Symfony/src/Ace/GenericBundle/Controller/EditorController.php
@@ -44,6 +44,8 @@ class EditorController extends Controller
 
 		$utilities = $this->get('utilities');
 
+		$boards = $utilities->get_boards();
+
 		$examples = json_decode($utilities->get_data($this->container->getParameter('library'), 'data', "builtin"), true);
 		$lib_examples = json_decode($utilities->get_data($this->container->getParameter('library'), 'data', "included"), true);
 		$extra_lib_examples = json_decode($utilities->get_data($this->container->getParameter('library'), 'data', "external"), true);
@@ -54,6 +56,6 @@ class EditorController extends Controller
 
 		// die(var_dump($examples)." ".var_dump($lib_examples)." ".var_dump($extra_lib_examples)." ");
 
-		return $this->render('AceGenericBundle:Editor:editor.html.twig', array('username'=>$name, 'project_id' => $id, 'project_name' => $name, 'examples' => $examples, 'lib_examples' => $lib_examples,'extra_lib_examples' => $extra_lib_examples, 'files' => $files));
+		return $this->render('AceGenericBundle:Editor:editor.html.twig', array('username'=>$name, 'project_id' => $id, 'project_name' => $name, 'examples' => $examples, 'lib_examples' => $lib_examples,'extra_lib_examples' => $extra_lib_examples, 'files' => $files, 'boards' => $boards));
 	}		
 }
