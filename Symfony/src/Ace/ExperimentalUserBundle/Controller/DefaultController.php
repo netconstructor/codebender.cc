ea52bb76696b6b3f3885ffbe3bfc5c234df47d51
Vasileios Georgitzikis <tzikis@gmail.com>
2012-10-30 04:53:24 +0200

    added experimentaluser service and getCurrentUser id fetching function

--- Symfony/src/Ace/ExperimentalUserBundle/Controller/DefaultController.php
+++ Symfony/src/Ace/ExperimentalUserBundle/Controller/DefaultController.php
@@ -3,13 +3,42 @@
 namespace Ace\ExperimentalUserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Security\Core\SecurityContext;
 
 
 class DefaultController extends Controller
 {
+	protected $sc;
+	protected $em;
     
     public function indexAction($name)
     {
         return $this->render('AceExperimentalUserBundle:Default:index.html.twig', array('name' => $name));
     }
+
+	public function getCurrentUserAction()
+	{
+		$response = array("success" => false);
+		$current_user = $this->sc->getToken()->getUser();
+		if($current_user !== "anon.")
+		{
+			$name = $current_user->getUsername();
+			$user = $this->em->getRepository('AceExperimentalUserBundle:ExperimentalUser')->findOneByUsername($name);
+			if (!$user)
+			{
+				throw $this->createNotFoundException('No user found with id '.$name);
+			}
+			$response = array("success" => true, "id" => $user->getId());
+		}
+		return new Response(json_encode($response));
+
+	}
+
+	public function __construct(SecurityContext $securityContext, EntityManager $entityManager)
+	{
+		$this->sc = $securityContext;
+	    $this->em = $entityManager;
+	}
 }
