15fe04e53062814edff735f333a6babc03eac0ed
Vasileios Georgitzikis <tzikis@gmail.com>
2012-11-20 00:45:41 +0200

    changed library and examples request from POST to GET

--- Symfony/src/Ace/GenericBundle/Controller/DefaultController.php
+++ Symfony/src/Ace/GenericBundle/Controller/DefaultController.php
@@ -94,7 +94,7 @@ class DefaultController extends Controller
 	{
 		$utilities = $this->get('utilities');
 
-		$libraries = json_decode($utilities->get_data($this->container->getParameter('library'), 'data', "all"), true);
+		$libraries = json_decode($utilities->get($this->container->getParameter('library')."?data=all"), true);
 		$categories = $libraries["categories"];
 
 		return $this->render('AceGenericBundle:Default:libraries.html.twig', array('categories' => $categories));
