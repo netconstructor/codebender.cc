498c049d041e89a197aabff2c76b50f5f3040909
Vasileios Georgitzikis <tzikis@gmail.com>
2012-12-11 21:48:11 +0200

    replaced old faulty tests with failing tests

--- Symfony/src/Ace/StaticBundle/Tests/Controller/DefaultControllerTest.php
+++ Symfony/src/Ace/StaticBundle/Tests/Controller/DefaultControllerTest.php
@@ -8,13 +8,14 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testPluginAction()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/static/plugin');
-
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("Firefox")')->count());
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("Google Chrome")')->count());
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("All Browsers - Windows & Mac")')->count());
+//        $client = static::createClient();
+//
+//        $crawler = $client->request('GET', '/static/plugin');
+//
+//        $this->assertGreaterThan(0, $crawler->filter('html:contains("Firefox")')->count());
+//        $this->assertGreaterThan(0, $crawler->filter('html:contains("Google Chrome")')->count());
+//        $this->assertGreaterThan(0, $crawler->filter('html:contains("All Browsers - Windows & Mac")')->count());
+		$this->assertTrue(false);
     }
 
 }
