a70a1e78ee10277f73c4c305a7111589cafaa2f8
Vasileios Georgitzikis <tzikis@gmail.com>
2012-12-12 00:43:48 +0200

    streamlined get_gravatar()

--- Symfony/src/Ace/UtilitiesBundle/Handler/DefaultHandler.php
+++ Symfony/src/Ace/UtilitiesBundle/Handler/DefaultHandler.php
@@ -57,17 +57,11 @@ class DefaultHandler
 	{
 		$d = 'mm';
 		$r = 'g';
-		$img = false;
-		$atts = array();
 
 		$url = 'http://www.gravatar.com/avatar/';
 		$url .= md5( strtolower( trim( $email ) ) );
 		$url .= "?s=$s&d=$d&r=$r";
-		if ( $img ) {
-			$url = '<img src="' . $url . '"';
-			foreach ( $atts as $key => $val ) $url .= ' ' . $key . '="' . $val . '"';
-			$url .= ' />';
-		}
+
 		return $url;
 	}
 
