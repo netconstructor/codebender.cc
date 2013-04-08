2c5d9cbb0019bd35fc5b8db9f4057def2f345a3e
m4rk <mark.orfanos@gmail.com>
2013-04-08 08:30:13 +0300

    moved registration inline field classes to form builder

--- Symfony/src/Ace/UserBundle/Form/Type/RegistrationFormType.php
+++ Symfony/src/Ace/UserBundle/Form/Type/RegistrationFormType.php
@@ -10,7 +10,21 @@ class RegistrationFormType extends BaseType
     public function buildForm(FormBuilder $builder, array $options)
     {
         parent::buildForm($builder, $options);
-
+		
+		// modify fos_user fields
+		$builder->get('username')->setAttribute('attr', array(
+															'placeholder' => 'Username',
+															'style' => 'max-width:100%'));
+		$builder->get('email')->setAttribute('attr', array(
+															'placeholder' => 'Email',
+															'style' => 'max-width:100%'));
+		$builder->get('plainPassword')->get('first')->setAttribute('attr', array(
+															'placeholder' => 'Type a Password',
+															'style' => 'max-width:100%'));
+		$builder->get('plainPassword')->get('second')->setAttribute('attr', array(
+															'placeholder' => 'Repeat Password',
+															'style' => 'max-width:100%'));
+																							
         // add your custom field
 		$builder
 			->add('firstname', 'text', array('label' => 'user_registration_form_firstname',	'required' => false))
