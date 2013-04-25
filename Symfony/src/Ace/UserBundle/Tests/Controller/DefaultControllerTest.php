49e454b5c15b62c9fcc25db999057edba19d7b4c
Vasileios Georgitzikis <tzikis@gmail.com>
2013-04-25 18:27:50 +0300

    added tests for existsAction, to make sure it works correctly both when the user exists and when they don't

--- Symfony/src/Ace/UserBundle/Tests/Controller/DefaultControllerTest.php
+++ Symfony/src/Ace/UserBundle/Tests/Controller/DefaultControllerTest.php
@@ -5,6 +5,57 @@ use Ace\UserBundle\Controller\DefaultController;
 
 class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 {
+	public function testExistsAction_Exists()
+	{
+		$user = $this->getMockBuilder('Ace\UserBundle\Entity\User')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$user->expects($this->once())->method('getId')->will($this->returnValue(1));
+		$user->expects($this->once())->method('getEmail')->will($this->returnValue("a@fake.email"));
+		$user->expects($this->once())->method('getUsername')->will($this->returnValue("iamfake"));
+		$user->expects($this->once())->method('getFirstname')->will($this->returnValue("fake"));
+		$user->expects($this->once())->method('getLastname')->will($this->returnValue("basterd"));
+		$user->expects($this->once())->method('getTwitter')->will($this->returnValue("atwitteraccount"));
+		$user->expects($this->once())->method('getKarma')->will($this->returnValue(150));
+		$user->expects($this->once())->method('getPoints')->will($this->returnValue(150));
+		$user->expects($this->once())->method('getReferrals')->will($this->returnValue(5));
+		$user->expects($this->once())->method('getReferrerUsername')->will($this->returnValue(null));
+		$user->expects($this->once())->method('getReferralCode')->will($this->returnValue(null));
+		$user->expects($this->once())->method('getWalkthroughStatus')->will($this->returnValue(0));
+
+		$repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+			->disableOriginalConstructor()
+			->setMethods(array("findOneByUsername"))
+			->getMock();
+
+		$repo->expects($this->once())->method('findOneByUsername')->with($this->equalTo("iamfake"))->will($this->returnValue($user));
+
+		$controller = $this->setUpController($templating, $security, $em, $container);
+
+		$em->expects($this->once())->method('getRepository')->with($this->equalTo('AceUserBundle:User'))->will($this->returnValue($repo));
+
+		$response = $controller->existsAction("iamfake");
+		$this->assertEquals($response->getContent(), 'true');
+	}
+
+	public function testExistsAction_NoUser()
+	{
+		$repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+			->disableOriginalConstructor()
+			->setMethods(array("findOneByUsername"))
+			->getMock();
+
+		$repo->expects($this->once())->method('findOneByUsername')->with($this->equalTo("idontexist"))->will($this->returnValue(null));
+
+		$controller = $this->setUpController($templating, $security, $em, $container);
+
+		$em->expects($this->once())->method('getRepository')->with($this->equalTo('AceUserBundle:User'))->will($this->returnValue($repo));
+
+		$response = $controller->existsAction("idontexist");
+		$this->assertEquals($response->getContent(), 'false');
+	}
+
 	public function testGetUserAction_UserExists()
 	{
 		$user = $this->getMockBuilder('Ace\UserBundle\Entity\User')
