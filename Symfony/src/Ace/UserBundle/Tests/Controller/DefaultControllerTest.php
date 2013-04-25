ff73eba807d6e27f0ec0800f6dbcb8efe29ca665
Vasileios Georgitzikis <tzikis@gmail.com>
2013-04-25 18:21:33 +0300

    added initializer for UserBundle Controller tests

--- Symfony/src/Ace/UserBundle/Tests/Controller/DefaultControllerTest.php
+++ Symfony/src/Ace/UserBundle/Tests/Controller/DefaultControllerTest.php
@@ -31,25 +31,10 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 
 		$repo->expects($this->once())->method('findOneByUsername')->with($this->equalTo("iamfake"))->will($this->returnValue($user));
 
-		$em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
-			->disableOriginalConstructor()
-			->getMock();
+		$controller = $this->setUpController($templating, $security, $em, $container);
 
 		$em->expects($this->once())->method('getRepository')->with($this->equalTo('AceUserBundle:User'))->will($this->returnValue($repo));
 
-		$templating = $this->getMockBuilder('Symfony\Bundle\TwigBundle\TwigEngine')
-			->setMethods(null)
-			->disableOriginalConstructor()
-			->getMock();
-
-		$security = $this->getMockBuilder('Symfony\Component\Security\Core\SecurityContext')
-			->setMethods(null)
-			->disableOriginalConstructor()
-			->getMock();
-
-		$container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
-
-		$controller = new DefaultController($templating, $security, $em, $container);
 		$response = $controller->getUserAction("iamfake");
 		$this->assertEquals($response->getContent(), '{"success":true,"id":1,"email":"a@fake.email","username":"iamfake","firstname":"fake","lastname":"basterd","twitter":"atwitteraccount","karma":150,"points":150,"referrals":5,"referrer_username":null,"referral_code":null,"walkthrough_status":0}');
 	}
@@ -63,11 +48,19 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 
 		$repo->expects($this->once())->method('findOneByUsername')->with($this->equalTo("idontexist"))->will($this->returnValue(null));
 
+		$controller = $this->setUpController($templating, $security, $em, $container);
+
+		$em->expects($this->once())->method('getRepository')->with($this->equalTo('AceUserBundle:User'))->will($this->returnValue($repo));
+
+		$response = $controller->getUserAction("idontexist");
+		$this->assertEquals($response->getContent(), '{"success":false}');
+	}
+
+	private function setUpController(&$templating, &$security, &$em, &$container)
+	{
 		$em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
 			->disableOriginalConstructor()
 			->getMock();
-
-		$em->expects($this->once())->method('getRepository')->with($this->equalTo('AceUserBundle:User'))->will($this->returnValue($repo));
 
 		$templating = $this->getMockBuilder('Symfony\Bundle\TwigBundle\TwigEngine')
 			->setMethods(null)
@@ -82,7 +75,6 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 		$container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
 
 		$controller = new DefaultController($templating, $security, $em, $container);
-		$response = $controller->getUserAction("idontexist");
-		$this->assertEquals($response->getContent(), '{"success":false}');
+		return $controller;
 	}
 }
