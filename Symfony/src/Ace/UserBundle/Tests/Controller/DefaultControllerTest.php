d105b5f4cb457db0438daa4af3b1c414907b5df7
Vasileios Georgitzikis <tzikis@gmail.com>
2013-04-26 17:48:00 +0300

    fixed tests for UserBundle's existsAction

--- Symfony/src/Ace/UserBundle/Tests/Controller/DefaultControllerTest.php
+++ Symfony/src/Ace/UserBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,38 +2,18 @@
 
 namespace Ace\UserBundle\Tests\Controller;
 use Ace\UserBundle\Controller\DefaultController;
+use Doctrine\ORM\Query;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 {
 	public function testExistsAction_Exists()
 	{
-		$user = $this->getMockBuilder('Ace\UserBundle\Entity\User')
-			->disableOriginalConstructor()
-			->getMock();
+		$this->initArguments($templating, $security, $em, $container);
 
-		$user->expects($this->once())->method('getId')->will($this->returnValue(1));
-		$user->expects($this->once())->method('getEmail')->will($this->returnValue("a@fake.email"));
-		$user->expects($this->once())->method('getUsername')->will($this->returnValue("iamfake"));
-		$user->expects($this->once())->method('getFirstname')->will($this->returnValue("fake"));
-		$user->expects($this->once())->method('getLastname')->will($this->returnValue("basterd"));
-		$user->expects($this->once())->method('getTwitter')->will($this->returnValue("atwitteraccount"));
-		$user->expects($this->once())->method('getKarma')->will($this->returnValue(150));
-		$user->expects($this->once())->method('getPoints')->will($this->returnValue(150));
-		$user->expects($this->once())->method('getReferrals')->will($this->returnValue(5));
-		$user->expects($this->once())->method('getReferrerUsername')->will($this->returnValue(null));
-		$user->expects($this->once())->method('getReferralCode')->will($this->returnValue(null));
-		$user->expects($this->once())->method('getWalkthroughStatus')->will($this->returnValue(0));
+		$controller = $this->getMock("Ace\UserBundle\Controller\DefaultController", array("getUserAction"), array($templating, $security, $em, $container));
 
-		$repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
-			->disableOriginalConstructor()
-			->setMethods(array("findOneByUsername"))
-			->getMock();
-
-		$repo->expects($this->once())->method('findOneByUsername')->with($this->equalTo("iamfake"))->will($this->returnValue($user));
-
-		$controller = $this->setUpController($templating, $security, $em, $container);
-
-		$em->expects($this->once())->method('getRepository')->with($this->equalTo('AceUserBundle:User'))->will($this->returnValue($repo));
+		$controller->expects($this->once())->method('getUserAction')->with($this->equalTo("iamfake"))->will($this->returnValue(new Response('{"success":true,"id":1,"email":"a@fake.email","username":"iamfake","firstname":"fake","lastname":"basterd","twitter":"atwitteraccount","karma":150,"points":150,"referrals":5,"referrer_username":null,"referral_code":null,"walkthrough_status":0}')));
 
 		$response = $controller->existsAction("iamfake");
 		$this->assertEquals($response->getContent(), 'true');
@@ -41,16 +21,11 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 
 	public function testExistsAction_NoUser()
 	{
-		$repo = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
-			->disableOriginalConstructor()
-			->setMethods(array("findOneByUsername"))
-			->getMock();
+		$this->initArguments($templating, $security, $em, $container);
 
-		$repo->expects($this->once())->method('findOneByUsername')->with($this->equalTo("idontexist"))->will($this->returnValue(null));
+		$controller = $this->getMock("Ace\UserBundle\Controller\DefaultController",array("getUserAction"), array($templating, $security, $em, $container));
 
-		$controller = $this->setUpController($templating, $security, $em, $container);
-
-		$em->expects($this->once())->method('getRepository')->with($this->equalTo('AceUserBundle:User'))->will($this->returnValue($repo));
+		$controller->expects($this->once())->method('getUserAction')->with($this->equalTo("idontexist"))->will($this->returnValue(new Response('{"success":false}')));
 
 		$response = $controller->existsAction("idontexist");
 		$this->assertEquals($response->getContent(), 'false');
@@ -234,7 +209,7 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals($response->getContent(), '{"success":false}');
 	}
 
-	private function setUpController(&$templating, &$security, &$em, &$container)
+	private function initArguments(&$templating, &$security, &$em, &$container)
 	{
 		$em = $this->getMockBuilder('Doctrine\ORM\EntityManager')
 			->disableOriginalConstructor()
@@ -250,8 +225,12 @@ class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 			->getMock();
 
 		$container = $this->getMock('Symfony\Component\DependencyInjection\ContainerInterface');
-
-		$controller = new DefaultController($templating, $security, $em, $container);
-		return $controller;
 	}
+
+	private function setUpController(&$templating, &$security, &$em, &$container)
+		{
+			$this->initArguments($templating, $security, $em, $container);
+			$controller = new DefaultController($templating, $security, $em, $container);
+			return $controller;
+		}
 }
