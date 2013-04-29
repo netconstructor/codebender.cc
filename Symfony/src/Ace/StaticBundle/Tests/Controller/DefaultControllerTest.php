f9daa9f16e384478ef811f53384bfc3b11481851
Vasileios Georgitzikis <tzikis@gmail.com>
2013-04-29 19:02:50 +0300

    added function test for team page

--- Symfony/src/Ace/StaticBundle/Tests/Controller/DefaultControllerTest.php
+++ Symfony/src/Ace/StaticBundle/Tests/Controller/DefaultControllerTest.php
@@ -18,4 +18,21 @@ class DefaultControllerTest extends WebTestCase
 		$this->assertTrue(false);
     }
 
+	public function testTeamAction()
+	{
+		$client = static::createClient();
+
+		$crawler = $client->request('GET', '/static/team');
+
+		$this->assertEquals(1, $crawler->filter('html:contains("Vasilis Georgitzikis")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("Stelios Tsampas")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("Dimitris Amaxilatis")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("Maria Kousta")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("Markellos Orfanos")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("Dimitris Dimakopoulos")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("Dimitrios Christidis")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("Alexandros Baltas")')->count());
+		$this->assertGreaterThanOrEqual(8, $crawler->filter('h2')->count());
+		$this->assertGreaterThanOrEqual(3, $crawler->filter('h1')->count());
+	}
 }
