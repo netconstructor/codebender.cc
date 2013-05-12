a991aac48f83ed5b20c60dd6c2392288dc8b1176
Vasileios Georgitzikis <tzikis@gmail.com>
2013-05-12 14:55:55 +0300

    added incomplete unit tests for ProjectBundle

---
+++ Symfony/src/Ace/ProjectBundle/Tests/Controller/FilesControllerUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ace\ProjectBundle\Tests\Controller;
+
+class FilesControllerUnitTest extends \PHPUnit_Framework_TestCase
+{
+	public function testUnitTested()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+}
---
+++ Symfony/src/Ace/ProjectBundle/Tests/Controller/MongoFilesControllerUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ace\ProjectBundle\Tests\Controller;
+
+class MongoFilesControllerUnitTest extends \PHPUnit_Framework_TestCase
+{
+	public function testUnitTested()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+}
