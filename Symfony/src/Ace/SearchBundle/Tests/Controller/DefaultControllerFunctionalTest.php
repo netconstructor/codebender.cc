0e94d8d165242143f150933121240abd74c15202
Vasileios Georgitzikis <tzikis@gmail.com>
2013-05-12 19:40:48 +0300

    added functional tests for SearchBundle

--- Symfony/src/Ace/SearchBundle/Tests/Controller/DefaultControllerFunctionalTest.php
+++ Symfony/src/Ace/SearchBundle/Tests/Controller/DefaultControllerFunctionalTest.php
@@ -6,8 +6,46 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerFunctionalTest extends WebTestCase
 {
-	public function testFunctionalTested()
+	public function testFindAction_None()
 	{
-		$this->markTestIncomplete('Not functional tested yet.');
+		$client = static::createClient();
+
+		$crawler = $client->request('GET', '/search/find/?query=nonexistent');
+
+		$this->assertEquals(1, $crawler->filter('html:contains("No results found :(")')->count());
+	}
+
+	public function testFindAction_UserExists()
+	{
+		$client = static::createClient();
+
+		$crawler = $client->request('GET', '/search/find/?query=tester');
+
+		$this->assertEquals(1, $crawler->filter('h2:contains("Users")')->count());
+		$this->assertEquals(0, $crawler->filter('h2:contains("Projects")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("myfirstname mylastname")')->count());
+	}
+
+	public function testFindAction_ProjectExists()
+	{
+		$client = static::createClient();
+
+		$crawler = $client->request('GET', '/search/find/?query=test_project');
+
+		$this->assertEquals(0, $crawler->filter('h2:contains("Users")')->count());
+		$this->assertEquals(1, $crawler->filter('h2:contains("Projects")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("tester")')->count());
+	}
+
+	public function testFindAction_BothExist()
+	{
+		$client = static::createClient();
+
+		$crawler = $client->request('GET', '/search/find/?query=test');
+
+		$this->assertEquals(1, $crawler->filter('h2:contains("Users")')->count());
+		$this->assertEquals(1, $crawler->filter('h2:contains("Projects")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("tester")')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("myfirstname mylastname")')->count());
 	}
 }
