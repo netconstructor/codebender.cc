fe55b2b7515a0fbe2206d5fc1db7b782cf531edd
Vasileios Georgitzikis <tzikis@gmail.com>
2013-05-12 14:12:57 +0300

    added failing tests for GenericBundle's EditorController

---
+++ Symfony/src/Ace/GenericBundle/Tests/Controller/EditorControllerFunctionalTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ace\GenericBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+
+class EditorControllerFunctionalTest extends WebTestCase
+{
+	public function testFunctionalTested()
+	{
+		$this->assertTrue(false);
+	}
+}
---
+++ Symfony/src/Ace/GenericBundle/Tests/Controller/EditorControllerUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ace\GenericBundle\Tests\Controller;
+
+class EditorControllerUnitTest extends \PHPUnit_Framework_TestCase
+{
+	public function testUnitTested()
+	{
+		$this->assertTrue(false);
+	}
+}
