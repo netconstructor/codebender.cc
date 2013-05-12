94bdae97979740b978cb8a838a7eb1dfe23e25c0
Vasileios Georgitzikis <tzikis@gmail.com>
2013-05-12 14:43:39 +0300

    added incomplete unit tests for Search and Sender

---
+++ Symfony/src/Ace/SearchBundle/Tests/Controller/DefaultControllerUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ace\SearchBundle\Tests\Controller;
+
+class DefaultControllerUnitTest extends \PHPUnit_Framework_TestCase
+{
+	public function testUnitTested()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+}
---
+++ Symfony/src/Ace/SenderBundle/Tests/Controller/DefaultControllerUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ace\SenderBundle\Tests\Controller;
+
+class DefaultControllerUnitTest extends \PHPUnit_Framework_TestCase
+{
+	public function testUnitTested()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+}
