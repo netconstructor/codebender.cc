954924de1b3e8f1865f0bdef04ca25590e1f9145
Vasileios Georgitzikis <tzikis@gmail.com>
2013-05-13 05:05:47 +0300

    explicitly defined the UtilitiesBundle's incomplete tests

--- Symfony/src/Ace/UtilitiesBundle/Tests/Controller/DefaultControllerUnitTest.php
+++ Symfony/src/Ace/UtilitiesBundle/Tests/Controller/DefaultControllerUnitTest.php
@@ -6,7 +6,87 @@ use Ace\UtilitiesBundle\Controller\BoardController;
 
 class DefaultControllerUnitTest extends \PHPUnit_Framework_TestCase
 {
-	public function testUnitTested()
+	public function testNewprojectAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testDeleteprojectAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testListFilenamesAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testGetDescriptionAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testSetDescriptionAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testSetNameAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testRenameFileAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testSidebarAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testDownloadAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testSaveCodeAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testCloneAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testCreateFileAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testDeleteFileAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testImageAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testUploadAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testUploadFilesAction()
+	{
+		$this->markTestIncomplete('Not unit tested yet.');
+	}
+
+	public function testLogAction()
 	{
 		$this->markTestIncomplete('Not unit tested yet.');
 	}
