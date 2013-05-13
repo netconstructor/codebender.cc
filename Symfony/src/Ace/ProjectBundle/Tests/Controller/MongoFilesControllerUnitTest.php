132eea41cc455ae70f0797a775c6b9a1ad62cd75
Alexandros Baltas <ampaltas@ceid.upatras.gr>
2013-05-13 05:03:26 +0300

    Added test for MongoFilesController createAction

--- Symfony/src/Ace/ProjectBundle/Tests/Controller/MongoFilesControllerUnitTest.php
+++ Symfony/src/Ace/ProjectBundle/Tests/Controller/MongoFilesControllerUnitTest.php
@@ -4,8 +4,32 @@ namespace Ace\ProjectBundle\Tests\Controller;
 
 class MongoFilesControllerUnitTest extends \PHPUnit_Framework_TestCase
 {
-	public function testUnitTested()
-	{
-		$this->markTestIncomplete('Not unit tested yet.');
-	}
+    protected $pf;
+	public function testCreateAction()
+    {
+        $controller = $this->setUpController($dm,NULL);
+        $dm->expects($this->once())->method("persist");
+        $dm->expects($this->once())->method('flush');
+
+        $response = $controller->createAction();
+        $this->assertEquals($response, '{"success":true,"id":null}');
+
+    }
+
+    protected function setUp()
+    {
+        $this->pf = $this->getMockBuilder('Ace\ProjectBundle\Document\ProjectFiles')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    private function setUpController(&$dm, $m)
+    {
+        $dm = $this->getMockBuilder('Doctrine\ODM\MongoDB\DocumentManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $controller = $this->getMock('Ace\ProjectBundle\Controller\MongoFilesController', $methods = $m, $arguments = array($dm));
+        return $controller;
+    }
 }
