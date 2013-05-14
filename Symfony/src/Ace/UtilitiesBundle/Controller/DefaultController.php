c5d0647758a11cab879bd16c1b0d68c3588ed28c
Vasileios Georgitzikis <tzikis@gmail.com>
2013-05-14 05:07:25 +0300

    added action to change a project's privacy

--- Symfony/src/Ace/UtilitiesBundle/Controller/DefaultController.php
+++ Symfony/src/Ace/UtilitiesBundle/Controller/DefaultController.php
@@ -77,6 +77,21 @@ class DefaultController extends Controller
 		return $this->render('AceUtilitiesBundle:Default:list_filenames.html.twig', array('files' => $files));
 	}
 
+	public function changePrivacyAction($id)
+	{
+		$projectmanager = $this->get('ace_project.sketchmanager');
+
+		$is_public = json_decode($projectmanager->getPrivacyAction($id)->getContent(), true);
+		$is_public = $is_public["response"];
+
+		if($is_public)
+			$response = $projectmanager->setProjectPrivateAction($id)->getContent();
+		else
+			$response = $projectmanager->setProjectPublicAction($id)->getContent();
+
+		return new Response($response);
+	}
+
 	public function getDescriptionAction($id)
 	{
 		$projectmanager = $this->get('ace_project.sketchmanager');
